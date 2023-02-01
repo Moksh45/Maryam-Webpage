ce3e326314befbe9d46a20efea7b211766535e61
Moksh Gupta <mokshgupta567@gmail.com>
2023-02-01 06:56:11 +0530

    add some changes in html css

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+let query = document.querySelector('.query');
+let submit = document.querySelector('.submit')
 
+submit.onclick = function()
+{
+    let url = "https://www.qwant.com/?q="+query.value+"&t=web";
+    window.open(url);
+}
   
